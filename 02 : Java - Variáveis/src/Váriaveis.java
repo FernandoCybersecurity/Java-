fa001bed3643e4d0b4ec98e63177cacb74651c02
FernandoCybersecurity <92273294+FernandoCybersecurity@users.noreply.github.com>
2021-11-10 22:00:55 -0300

    Add files via upload

---
+++ 02 : Java - Variáveis/src/Váriaveis.java
@@ -0,0 +1,9 @@
+public class Váriaveis {
+    public static void main(String[] args) {
+        String nome = "Fernando";
+        Integer soma
+        //Processamento
+
+        String a = "",b = "", c, d;
+    }
+}
