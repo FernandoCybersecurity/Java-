83d33d80b0bc3b628251bfc9c514405e9483af60
FernandoCybersecurity <92273294+FernandoCybersecurity@users.noreply.github.com>
2021-11-10 22:02:38 -0300

    Delete 02 : Java - Variáveis directory

--- 02 : Java - Variáveis/src/Váriaveis.java
+++
@@ -1,9 +0,0 @@
-public class Váriaveis {
-    public static void main(String[] args) {
-        String nome = "Fernando";
-        Integer soma
-        //Processamento
-
-        String a = "",b = "", c, d;
-    }
-}
