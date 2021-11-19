52166dd81479c5db6138b7f47e1a9e88c82143c9
FernandoCybersecurity <92273294+FernandoCybersecurity@users.noreply.github.com>
2021-11-19 20:10:12 -0300

    Add files via upload

---
+++ 05 : ManipulacaodeStrings/ManipulacaoStrings.java
@@ -0,0 +1,17 @@
+public class ManipulacaoStrings{
+    public static void main(String[] args) {
+        //Manipulação de strings
+        // Código importante: nome.charAt(0)
+        String nome = "Java";
+        System.out.println("Primeiro caracter: " + nome.charAt(0));
+
+        String sobrenome = "Rules";
+        System.out.println(nome + " " + sobrenome);
+
+        Integer idade = 10;
+        System.out.println("Idade: " + idade);
+
+
+
+    }
+}
