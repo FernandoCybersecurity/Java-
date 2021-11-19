581d5b89ee4b2d7ab487d79278c2cb37519fcdcf
FernandoCybersecurity <92273294+FernandoCybersecurity@users.noreply.github.com>
2021-11-19 13:14:13 -0300

    Add files via upload

---
+++ 03 : IncrementoDecremento/IncrementoDecremento.java
@@ -0,0 +1,9 @@
+public class IncrementoDecremento{
+    public static void main(String[] args) {
+        
+        Integer numero = 10;
+        System.out.println(numero++);
+        System.out.println(++numero);
+
+    }
+}
